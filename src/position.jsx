f60243921b5ba3f32f94a521c1a02ded7fd5ea75
Ethan Burkinshaw <61204146+Snowworm3000@users.noreply.github.com>
2024-11-02 19:36:15 +0000

    Created position class to get the current GPS coordinates

--- src/position.jsx
+++ src/position.jsx
@@ -1,8 +1,5 @@
-import React, { Component } from 'react';
-
-class Position extends Component {
+class Position {
     constructor(props) {
-        super(props);
         this.state = {
             latitude: null,
             longitude: null,
@@ -10,40 +7,25 @@ class Position extends Component {
         };
     }
 
-    componentDidMount() {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(
-                (position) => {
-                    this.setState({
-                        latitude: position.coords.latitude,
-                        longitude: position.coords.longitude
-                    });
-                },
-                (error) => {
-                    this.setState({ error: error.message });
-                }
-            );
-        } else {
-            this.setState({ error: 'Geolocation is not supported by this browser.' });
-        }
-    }
-
-    render() {
-        const { latitude, longitude, error } = this.state;
-
-        return (
-            <div>
-                <h1>Position Coordinates</h1>
-                {error ? (
-                    <p>Error: {error}</p>
-                ) : (
-                    <div>
-                        <p>Latitude: {latitude}</p>
-                        <p>Longitude: {longitude}</p>
-                    </div>
-                )}
-            </div>
-        );
+    getCoords() {
+        return new Promise((resolve, reject) => {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(
+                    (position) => {
+                        const coords = {
+                            latitude: position.coords.latitude,
+                            longitude: position.coords.longitude
+                        };
+                        resolve(coords);
+                    },
+                    (error) => {
+                        reject(error.message);
+                    }
+                );
+            } else {
+                reject('Geolocation is not supported by this browser.');
+            }
+        });
     }
 }
 
